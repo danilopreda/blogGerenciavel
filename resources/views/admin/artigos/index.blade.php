80192663281878cc634dd7a58ae5e9f02ee562c9
Danilo Preda <danilopreda@live.com>
2018-10-12 00:48:21 -0300

    Formulário

--- resources/views/admin/artigos/index.blade.php
+++ resources/views/admin/artigos/index.blade.php
@@ -4,6 +4,8 @@
     <pagina tamanho="12">
         <painel titulo="Lista de Artigos">
             <migalhas v-bind:lista="{{$listaMigalhas}}"></migalhas>
+
+            <modallink tipo="link" nome="meuModalTeste" titulo="Criar" css=""></modallink>
             <tabela-lista
             v-bind:titulos="['teste', 'outros']"
             v-bind:itens="[[1,'PHP OO', 'Curso de PHO OO'], [2, 'Vue JS', 'Curso de Vue JS']]"
@@ -13,4 +15,21 @@
 
             </painel>
     </pagina>
+
+    <modal nome="meuModalTeste">
+    <painel titulo="Adicionar">
+        <formulario css="" action="#" method="put" enctype="multipart/form-data" token="12345">
+            <div class="form-group">
+                <label for="titulo">Título</label>
+                <input type="text" class="form-control" id="titulo" name="titulo" placeholder="Título">
+            </div>
+
+            <div class="form-group">
+                <label for="descricao">Descrição</label>
+                <input type="text" class="form-control" id="descricao" name="descricao" placeholder="Descrição">
+            </div>
+            <button class="btn btn-info">Adicionar</button>
+        </formulario>
+    </painel>
+    </modal>
 @endsection
